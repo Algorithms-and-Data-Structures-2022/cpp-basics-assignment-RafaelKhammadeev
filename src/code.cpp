2c1d833ab56027a53dbcb4b241c90091d76f390f
Leaflet213 <msmk.swimmer@gmail.com>
2022-03-04 12:53:50 +0300

    add some change and task 4

--- src/code.cpp
+++ src/code.cpp
@@ -8,9 +8,9 @@ namespace assignment {
 
   // Task 1
   int squared_diff(int left, int right) {
-    int result = (left - right)*(left - right);
-    cout << result << endl;
-    return 0;
+    int diff = left - right;
+    int result = diff * diff;
+    return result;
   }
 
   // Task 2
@@ -20,19 +20,23 @@ namespace assignment {
 
     return false;
   }
-
+  // << с помощью сдвига
   // Task 3
   int max3(int left, int middle, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    int i;
+    int massive[] = {left, middle, right};
+    int result = massive[0];
+    for (i = 0; i < 3; i++){
+      result = max(result, massive[i]);
+    }
+    return result;
   }
 
   // Task 4
   void swap_args(int *left, int *right) {
-
-    // Write your code here ...
+    int change = *left;
+    *left = *right;
+    *right = change;
   }
 
   // Task 5
